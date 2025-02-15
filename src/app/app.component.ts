8340ce46ea3c63bbb8d8d4043ca81296b0a52c82
Michael Doyle <HiReception@users.noreply.github.com>
2025-02-15 23:34:39 +1100

    Add Settings menu with first option

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SettingsButtonComponent } from "./settings-button/settings-button.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    SettingsButtonComponent
+],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/settings-button/settings-button.component.ts
@@ -0,0 +1,31 @@
+import { Component, inject } from '@angular/core';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatRadioModule } from '@angular/material/radio';
+import { LocalStorageService } from '../local-storage.service';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-settings-button',
+  imports: [
+    MatRadioModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    FormsModule,
+  ],
+  templateUrl: './settings-button.component.html',
+  styleUrl: './settings-button.component.css'
+})
+export class SettingsButtonComponent {
+  localStorageService = inject(LocalStorageService);
+
+  updateSettings(key: string, value: any) {
+    const existingSettings = this.localStorageService.settings();
+    this.localStorageService.settings.set({
+      ...existingSettings,
+      [key]: value
+    })
+  }
+}
